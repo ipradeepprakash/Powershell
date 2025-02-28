0a2035699897a38ab48ae1f67d6b09d141c72785
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2025-02-28 16:42:06 +0530

    changes
    changes

---
+++ SEARCH and kill the PROCESS from TASK MANAGER.ps1
@@ -0,0 +1,8 @@
+﻿# kill DTS package job
+
+# step4: check the DTS name #cm_ssis_# & enter in command line '#cm_55is_#' as shown search execute the below command
+Get-WmiObject -class win32_process | Where-Object {$_.commandline -like '*CM_SSIS_ACCESS_LOG*'} |Select {S _. processID}, {$ _. processname}, {$ _. CommandLine} |fl
+
+# step5: kill the process id
+Stop-Process -Id < Process_ID > -Confirm -PassThruhostname
+
